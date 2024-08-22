fd33ab271da42d5862dfd55300bac2b4f8c661cc
Ravdeep Singh <mail@ravdeep.com>
2024-08-22 15:19:56 +0200

    cleanup chrome extension code

--- devtools.js
+++ devtools.js
@@ -3,16 +3,3 @@ chrome.devtools.panels.elements.createSidebarPane(
       sidebar.setPage("panel.html");
   }
 );
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.addEventListener('click', (event) => {
-    const clickedElement = event.target;
-   // const selector = buildSelector(clickedElement);
-
-    console.log(clickedElement);
-  //  console.log(selector);
-  //  let selectedElement = document.getElementById("selected_element");
-  //  selectedElement.value = selector;
-  });
-})
-
